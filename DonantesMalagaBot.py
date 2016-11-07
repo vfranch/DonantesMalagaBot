942218687515ebe3956ec7612f2de864ca42e7b0
vfranch <vfranch@theworkshop.com>
2016-11-07 12:52:20 +0100

    Add meta-info for the time to start/remaining time to donate in each spot. Add some documentation.

--- DonantesMalagaBot.py
+++ DonantesMalagaBot.py
@@ -28,6 +28,9 @@ incoming_info_cached = ""
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
+    '''
+    Display the commands and what are they intended for.
+    '''
     bot.reply_to(message, build_help_message())
     markup = types.ReplyKeyboardMarkup()
     item_dondehoy = types.KeyboardButton('/dondehoy')
@@ -40,6 +43,9 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['dondehoy'])
 def get_locations_today(message):
+    '''
+    Retrieve data about where and at what time to donate today.
+    '''
     bot.send_chat_action(message.chat.id, 'typing')
     global last_cached_date_today
     global today_info_cached
@@ -49,7 +55,7 @@ def get_locations_today(message):
         today_info = download_locations_today()
         if len(today_info) > 0:
             # send info
-            bot.reply_to(message, today_info)
+            bot.reply_to(message, to_string(today_info))
             # update last cached date for today
             last_cached_date_today = today_str
             today_info_cached = today_info
@@ -58,11 +64,14 @@ def get_locations_today(message):
             bot.reply_to(message, "Parece que hoy no se puede donar (o los datos aun no ha sido publicados)")
     else:
         # return cached data
-        bot.reply_to(message, today_info_cached)
+        bot.reply_to(message, to_string(today_info_cached))
         print("Returned today info from cache")
 
 @bot.message_handler(commands=['dondeproximamente'])
 def get_incoming_locations(message):
+    '''
+    Retrieve data about where and when to donate in the incoming days.
+    '''
     chat_id = message.chat.id
     bot.send_chat_action(chat_id, 'typing')
     global last_cached_date_incoming
@@ -83,11 +92,17 @@ def get_incoming_locations(message):
     
 @bot.message_handler(commands=['hedonadohoy'])
 def create_donation_checkpoint(message):
+    '''
+    Persist that the user donated today so that the period of 2 months for next donation starts counting.
+    '''
     execute_insert("INSERT INTO donations (user_id, last_donation_unix_timestamp, notified) VALUES({0}, UNIX_TIMESTAMP(), 0) ON DUPLICATE KEY UPDATE user_id={0}, last_donation_unix_timestamp=UNIX_TIMESTAMP(), notified=0".format(message.from_user.id))
     bot.reply_to(message, "De acuerdo, queda anotado que has donado el {0}".format(time.strftime("%d/%m/%Y")))
     
 @bot.message_handler(commands=['puedodonar'])
 def can_donate_today(message):
+    '''
+    Check whether the donor can donate or not based on the last donation date and the fact that more than 2 month should have passed.
+    '''
     connection = get_db_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT FROM_UNIXTIME(last_donation_unix_timestamp, '%d/%m/%Y') FROM donations WHERE user_id={0}".format(message.from_user.id))
@@ -104,28 +119,30 @@ def can_donate_today(message):
     else:
        bot.reply_to(message, "No se si puedes donar porque no tengo constancia de cuando fue la ultima vez que lo hiciste.") 
     cursor.close()
-    close_db_connection(connection)
+    connection.close()
 
-# Parses today info HTML page
 def parse_today_page(html):
-    parsed_string = ""
+    '''
+    Parses today info HTML page
+    '''
+    today_data = []
     soup = BeautifulSoup(html, "lxml")
     table = soup.find('table')
     table_body = table.find('tbody')
     rows = table_body.find_all('tr')[2:-1]
     for row in rows:
         columns = row.find_all('td')
-        location = parse_location_column(columns[1])
+        location = columns[1].text.title().strip()
         info = parse_info_column(columns[2])
-        parsed_string += "- {0} ({1})\n".format(info, location)
+        today_data.append({'location': location, 'start': info['start'], 'end': info['end'], 'description': info['info']})
     
-    if len(parsed_string) == 0:
-        return "";
-    else:
-        return "Hoy puedes donar en los siguientes lugares:\n\n" + parsed_string
+    return today_data
 
-# Parses incoming info HTML page
 def parse_incoming_page(html):
+    '''
+    Parses incoming info HTML page
+    '''
+    today_str = time.strftime("%d/%m/%Y")
     parsed_string = "Proximamente puedes donar en los siguientes lugares:\n\n"
     soup = BeautifulSoup(html, "lxml")
     table = soup.find('table')
@@ -133,25 +150,41 @@ def parse_incoming_page(html):
     for row in rows:
         columns = row.find_all('td')
         date = columns[0].text.strip()
-        location = parse_location_column(columns[1])
+        if date == today_str:
+            continue # Ignores the line if incoming date is today
+        location = columns[1].text.title().strip()
         info = parse_info_column(columns[2])
-        parsed_string += "- {0}: {1} ({2})\n".format(date, info, location)
+        parsed_string += "- {0}: {1} ({2})\n".format(date, info['info'], location)
     
     return parsed_string
 
-# Parses the location column by just stripping it
-def parse_location_column(location):
-    return location.text.title().strip()
-
-# Parses the info column by removing line feeds and the date in human readable format
 def parse_info_column(info):
+    '''
+    Parses the info column by removing line feeds and the date in human readable format
+    '''
     titled = info.text.title()
-    no_date = re.sub(r'^(\W*\w+\W*){3}', '', titled)
-    no_linefeeds = no_date.replace("\n", " ")
-    return no_linefeeds
+    # remove line feeds from description
+    no_linefeeds = titled.replace("\n", " ")
+    # remove date from the description
+    no_date = re.sub(r'^(\W*\w+\W*){3}', '', no_linefeeds)
+    # extract start/end time
+    match = re.match(r'.*De (\d+) A (\d+) Horas', no_date)
+    start_time = match.group(1)
+    end_time = match.group(2)
+    # extracts data
+    match = re.match(r'.*\((.*)\).*', no_date)
+    if match is None:
+        # Brackets not found, no tansformation needed
+        info = no_date
+    else:
+        real_location = match.group(1)
+        info = "{0} De {1} A {2}".format(real_location, start_time, end_time)
+    return {'start': start_time, 'end': end_time, 'info': info}
     
-# Sends message expected to be long by smartly splitting it
 def send_message_splitting_if_necessary(chat_id, long_text):
+    '''
+    Sends message expected to be long by smartly splitting it
+    '''
     lines = long_text.split('\n')
     current_text = ""
     for line in lines:
@@ -160,25 +193,27 @@ def send_message_splitting_if_necessary(chat_id, long_text):
             bot.send_message(chat_id, current_text)
             current_text = ""
 
-# Helper method to establish db connection
 def get_db_connection():
+    '''
+    Helper method to establish db connection
+    '''
     return MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWORD, db=DB_NAME)
-    
-# Helper method to close db connection
-def close_db_connection(connection):
-    connection.close()
 
-# Executes db insert statement with all the boiler-plate code solved
 def execute_insert(statement):
+    '''
+    Executes db insert statement with all the boiler-plate code solved
+    '''
     connection = get_db_connection()
     cursor = connection.cursor()
     cursor.execute(statement)
     connection.commit()
     cursor.close()
-    close_db_connection(connection)
+    connection.close()
 
-# Downloads today info page and parses it to a string
 def download_locations_today():
+    '''
+    Download today info page and parses it to a string
+    '''
     r = requests.get(TODAY_URL)
     if r.status_code == 200:
         return parse_today_page(r.text)
@@ -186,17 +221,52 @@ def download_locations_today():
         return "Parece que esa informacion no esta disponible en estos momentos. \
         Por favor, intentalo de nuevo mas tarde"
 
-# Donwnload incoming info page and parses it to a (large) string
 def download_locations_incoming():
+    '''
+    Download incoming info page and parses it to a (large) string
+    '''
     r = requests.get(INCOMING_URL)
     if r.status_code == 200:
         return parse_incoming_page(r.text)
     else:
         return "Hummm, parece que esa informacion no esta disponible en estos momentos. \
         Por favor, intentalo de nuevo mas tarde"
+        
+def to_string(spots):
+    '''
+    Print data in human readable format, adding metadata when useful.
+    '''
+    current_hour = int(time.strftime("%H")) + 1
+    string = "Hoy puedes donar en los siguientes lugares:\n\n"
+    for spot in spots:
+        status = calculate_status(current_hour, int(spot['start']), int(spot['end']))
+        string += "- {0} ({1}) [{2}]\n".format(spot['description'], spot['location'], status)
+    
+    return string
+    
+def calculate_status(current_hour, start, finish):
+    '''
+    Calculates the status of a donation spot based on the current time and the spot start/finish times.
+    '''
+    if current_hour < start:
+        time_to_start = start - current_hour
+        if time_to_start > 0:
+            return "Faltan {0} horas".format(time_to_start)
+        else:
+            return "Empieza en breve"
+    elif current_hour < finish:
+        time_left = finish - current_hour
+        if time_left > 0:
+            return "Disponible, termina en {0} horas".format(time_left)
+        else:
+            return "Disponible, termina en breve"
+    else:
+        return "Ya ha finalizado"
 
-# Helper method to build the bot help message
 def build_help_message():
+    '''
+    Helper method to build the bot help message
+    '''
     return "\n \
 DonantesMalagaBot facilita informacion a los donantes de Malaga acerca de los lugares donde se puede donar en el dia de hoy o en los proximos dias.\n \
 \n \
@@ -208,4 +278,8 @@ Los comandos para interactuar con el bot son: \n \
 /puedodonar - permite saber si puedo donar hoy en base a la fecha de ultima donacion conocida \n \
 \n"
     
-bot.polling(none_stop=True)
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except:
+        print("Telegram API timeout happened")
